77f5ce88d21b3ddf9fb4c257027d44f7f176a18f
Muhammad Nauman <nauman33183@gmail.com>
2025-03-20 14:37:06 +0500

    updates in navigation

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { Text } from "react-native";
 import Toast from "react-native-toast-message";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Icon } from "react-native-vector-icons/Icon";
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 // Lazy Load Screens
 const Auth = lazy(() => import("./src/screens/Auth"));
@@ -17,6 +19,9 @@ const Home = lazy(() => import("./src/screens/Home"));
 const Profile = lazy(() => import("./src/screens/Profile"));
 const Vehicles = lazy(() => import("./src/screens/Vehicles"));
 const Auctions = lazy(() => import("./src/screens/Auctions"));
+const Dashboard = lazy(() => import("./src/screens/Dashboard"));
+const Wallet = lazy(() => import("./src/screens/Wallet"));
+const Orders = lazy(() => import("./src/screens/Orders"));
 
 export type RootStackParamList = {
   Auth: undefined;
@@ -29,6 +34,7 @@ export type RootStackParamList = {
 };
 
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   const [token, setToken] = useState<string | null>(null);
@@ -83,21 +89,28 @@ const UnauthenticatedTabs = ({ setToken }: { setToken: React.Dispatch<React.SetS
 
 // Tabs for Authenticated Users (Bottom Navigation Visible)
 const AuthenticatedTabs = () => (
-  <Tab.Navigator
-    screenOptions={{
-      headerLeft: () => (
-        <Icon name="menu" size={30} />
-      ),
-      headerRight: () => (
-        <Icon name="bell" size={30} />
-      )
-    }}
-    initialRouteName="Home">
-    <Tab.Screen name="Home" component={LazyWrapper(Home)} />
-    <Tab.Screen name="Profile" component={LazyWrapper(Profile)} />
-    <Tab.Screen name="Vehicles" component={LazyWrapper(Vehicles)} />
-    <Tab.Screen name="Auctions" component={LazyWrapper(Auctions)} />
-  </Tab.Navigator>
+  <>
+    <Tab.Navigator
+      screenOptions={{
+        headerLeft: () => (
+          <Icon name="menu" size={30} />
+        ),
+        headerRight: () => (
+          <Icon name="bell" size={30} />
+        )
+      }}
+      initialRouteName="Home">
+      <Tab.Screen name="Home" component={LazyWrapper(Home)} />
+      <Tab.Screen name="Profile" component={LazyWrapper(Profile)} />
+      <Tab.Screen name="Vehicles" component={LazyWrapper(Vehicles)} />
+      <Tab.Screen name="Auctions" component={LazyWrapper(Auctions)} />
+    </Tab.Navigator>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Dashboard" component={Dashboard} />
+      <Drawer.Screen name="Wallet" component={Wallet} />
+      <Drawer.Screen name="Orders" component={Orders} />
+    </Drawer.Navigator>
+  </>
 );
 
 // Function to wrap lazy-loaded components with Suspense
